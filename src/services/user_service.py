fe42ad4ccfc09d28930aeb378ae8c45378857cbe
Aimy <cherry051725@gmail.com>
2017-01-14 15:20:19 +0000

    DB更新系のAPIを追加しました。

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,11 +1,12 @@
 from models import db
 from models.user import User
 
+
 class UserService:
-	
+
     def __init__(self):
         pass
-	
+
     def get_user_list(self) -> dict:
         """
         全ユーザリストを取得
@@ -16,3 +17,13 @@ class UserService:
         for user in users:
             user_list[user.id] = user.name
         return user_list
+
+    def add_user(self, name: str):
+        """
+        ユーザの追加
+        :param str name: user name
+        :return:
+        """
+        user = User(name)
+        db.session.add(user)
+        db.session.flush()
